1030c4ed81793746fc40623f818c1ba0a5ed780f
manfredi.marceca <manfredi.marceca@outlook.it>
2024-05-13 20:16:24 +0200

    Added win-arm64 and linux-x86

--- tools/get_pdfium.sh
+++ tools/get_pdfium.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-linux-x64.tgz
+wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-linux-x86.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-linux-arm.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-linux-arm64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-win-x64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-win-x86.tgz
+wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-win-arm64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-mac-x64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F5445/pdfium-mac-arm64.tgz
 
-mkdir linux
+mkdir linux-x64
+mkdir linux-x86
 mkdir linux-arm
 mkdir linux-arm64
-mkdir windows
-mkdir windowsx86
+mkdir win-x64
+mkdir win-x86
+mkdir win-arm64
 mkdir osx-x64
 mkdir osx-arm64
 
-tar -xvf pdfium-linux-x64.tgz -C linux
+tar -xvf pdfium-linux-x64.tgz -C linux-x64
+tar -xvf pdfium-linux-x86.tgz -C linux-x86
 tar -xvf pdfium-linux-arm.tgz -C linux-arm
 tar -xvf pdfium-linux-arm64.tgz -C linux-arm64
 tar -xvf pdfium-mac-x64.tgz -C osx-x64
 tar -xvf pdfium-mac-arm64.tgz -C osx-arm64
-tar -xvf pdfium-win-x64.tgz -C windows
-tar -xvf pdfium-win-x86.tgz -C windowsx86
+tar -xvf pdfium-win-x64.tgz -C win-x64
+tar -xvf pdfium-win-x86.tgz -C win-x86
+tar -xvf pdfium-win-arm64.tgz -C win-arm64
 
-mkdir -p ../src/Docnet.Core/runtimes/linux/native/
+mkdir -p ../src/Docnet.Core/runtimes/linux-x64/native/
+mkdir -p ../src/Docnet.Core/runtimes/linux-x86/native/
 mkdir -p ../src/Docnet.Core/runtimes/linux-arm/native/
 mkdir -p ../src/Docnet.Core/runtimes/linux-arm64/native/
 mkdir -p ../src/Docnet.Core/runtimes/osx-x64/native/
 mkdir -p ../src/Docnet.Core/runtimes/osx-arm64/native/
 mkdir -p ../src/Docnet.Core/runtimes/win-x64/native/
 mkdir -p ../src/Docnet.Core/runtimes/win-x86/native/
+mkdir -p ../src/Docnet.Core/runtimes/win-arm64/native/
 
-cp linux/lib/libpdfium.so ../src/Docnet.Core/runtimes/linux/native/pdfium.so
-cp linux/LICENSE ../src/Docnet.Core/runtimes/linux/native/LICENSE
+cp linux-x64/lib/libpdfium.so ../src/Docnet.Core/runtimes/linux-x64/native/pdfium.so
+cp linux-x64/LICENSE ../src/Docnet.Core/runtimes/linux-x64/native/LICENSE
+
+cp linux-x86/lib/libpdfium.so ../src/Docnet.Core/runtimes/linux-x86/native/pdfium.so
+cp linux-x86/LICENSE ../src/Docnet.Core/runtimes/linux-x86/native/LICENSE
 
 cp linux-arm/lib/libpdfium.so ../src/Docnet.Core/runtimes/linux-arm/native/pdfium.so
 cp linux-arm/LICENSE ../src/Docnet.Core/runtimes/linux-arm/native/LICENSE
@@ -47,11 +58,14 @@ cp osx-x64/LICENSE ../src/Docnet.Core/runtimes/osx-x64/native/LICENSE
 cp osx-arm64/lib/libpdfium.dylib ../src/Docnet.Core/runtimes/osx-arm64/native/pdfium.dylib
 cp osx-arm64/LICENSE ../src/Docnet.Core/runtimes/osx-arm64/native/LICENSE
 
-cp windows/bin/pdfium.dll ../src/Docnet.Core/runtimes/win-x64/native/pdfium.dll
-cp windows/LICENSE ../src/Docnet.Core/runtimes/win-x64/native/LICENSE
+cp win-x64/bin/pdfium.dll ../src/Docnet.Core/runtimes/win-x64/native/pdfium.dll
+cp win-x64/LICENSE ../src/Docnet.Core/runtimes/win-x64/native/LICENSE
 
-cp windowsx86/bin/pdfium.dll ../src/Docnet.Core/runtimes/win-x86/native/pdfium.dll
-cp windowsx86/LICENSE ../src/Docnet.Core/runtimes/win-x86/native/LICENSE
+cp win-x86/bin/pdfium.dll ../src/Docnet.Core/runtimes/win-x86/native/pdfium.dll
+cp win-x86/LICENSE ../src/Docnet.Core/runtimes/win-x86/native/LICENSE
 
-rm pdfium-linux-x64.tgz pdfium-linux-arm.tgz pdfium-linux-arm64.tgz pdfium-win-x64.tgz pdfium-win-x86.tgz pdfium-mac-x64.tgz pdfium-mac-arm64.tgz
-rm -rf linux linux-arm linux-arm64 windows windowsx86 osx-x64 osx-arm64
+cp win-arm64/bin/pdfium.dll ../src/Docnet.Core/runtimes/win-arm64/native/pdfium.dll
+cp win-arm64/LICENSE ../src/Docnet.Core/runtimes/win-arm64/native/LICENSE
+
+rm pdfium-linux-x64.tgz pdfium-linux-x86.tgz pdfium-linux-arm.tgz pdfium-linux-arm64.tgz pdfium-win-x64.tgz pdfium-win-x86.tgz pdfium-win-arm64.tgz pdfium-mac-x64.tgz pdfium-mac-arm64.tgz
+rm -rf linux-x64 linux-x86 linux-arm linux-arm64 win-x64 win-x86 win-arm64 osx-x64 osx-arm64
